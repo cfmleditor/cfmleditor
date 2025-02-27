e1f3ae4bd86982186fefe804774f66e7e5634c57
Gareth Edwards <ghedwards@gmail.com>
2025-02-27 11:00:58 +1000

    Manual merges from "stylistic" ( bit of a mess )

--- src/entities/keyword.ts
+++ src/entities/keyword.ts
@@ -1,72 +1,72 @@
 export interface KeywordDetails {
-  description: string;
-  onlyScript: boolean;
-  links: string[];
+	description: string;
+	onlyScript: boolean;
+	links: string[];
 }
 
 export interface Keywords {
-  [keyword: string]: KeywordDetails;
+	[keyword: string]: KeywordDetails;
 }
 
 export const keywords: Keywords = {
-  "var": {
-    description: "",
-    onlyScript: false,
-    links: []
-  },
-  "for": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "default": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "continue": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "import": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "finally": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "interface": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "pageencoding": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "abort": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "exist": {
-    description: "",
-    onlyScript: true,
-    links: []
-  },
-  "true": {
-    description: "",
-    onlyScript: false,
-    links: []
-  },
-  "false": {
-    description: "",
-    onlyScript: false,
-    links: []
-  },
+	var: {
+		description: "",
+		onlyScript: false,
+		links: [],
+	},
+	for: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	default: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	continue: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	import: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	finally: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	interface: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	pageencoding: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	abort: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	exist: {
+		description: "",
+		onlyScript: true,
+		links: [],
+	},
+	true: {
+		description: "",
+		onlyScript: false,
+		links: [],
+	},
+	false: {
+		description: "",
+		onlyScript: false,
+		links: [],
+	},
 };
