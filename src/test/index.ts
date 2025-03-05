4f3cfb689b1ac00f990cce7896962a6489ab9f49
Gareth Edwards <ghedwards@gmail.com>
2025-03-05 12:05:48 +1000

    Testing adding test to project

---
+++ src/test/index.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+
+import { glob } from "glob";
+import Mocha from "mocha";
+
+/**
+ * !: must be synchronized
+ * @param testsRoot
+ * @param cb
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function run(testsRoot: string, cb: (error: any, failures?: number) => void): void {
+	const mocha = new Mocha({ color: true });
+
+	glob("**/suite/**.test.js", { cwd: testsRoot })
+		.then((files) => {
+			for (const f of files) {
+				console.log(f);
+				mocha.addFile(path.resolve(testsRoot, f));
+			}
+
+			try {
+				mocha.run((failures) => {
+					cb(null, failures);
+				});
+			}
+			catch (error) {
+				cb(error);
+			}
+		})
+		.catch(error => cb(error));
+}
---
+++ src/test/runTest.ts
@@ -0,0 +1,17 @@
+import { resolve } from "node:path";
+
+import { runTests } from "@vscode/test-electron";
+
+void (async function go() {
+	const projectPath = resolve(__dirname, "../");
+	const extensionDevelopmentPath = projectPath;
+	const extensionTestsPath = resolve(projectPath, "./out/test");
+	// const testWorkspace = resolve(projectPath, "./test-workspace");
+
+	await runTests({
+		version: "insiders",
+		extensionDevelopmentPath,
+		extensionTestsPath,
+		launchArgs: ["--disable-extensions"],
+	});
+})();
