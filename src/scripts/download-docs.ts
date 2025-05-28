4d431a0bd70e4c83100eb78223f1d882623393fc
Thomas Rafferty <thomas.rafferty.256@gmail.com>
2025-05-29 00:29:40 +1000

    Backup docs when updates are available

--- src/scripts/download-docs.ts
+++ src/scripts/download-docs.ts
@@ -5,23 +5,55 @@ import { rimrafSync } from "rimraf";
 import JSZip from "jszip";
 import assert from "assert";
 
-const rootDir = path.join(__dirname, "..", "..");
-
 async function main() {
-	// Remove the existing docs folder if it exists
-	const docsFolder = path.join(rootDir, "resources", "docs");
-	if (fs.existsSync(docsFolder)) {
-		if (!rimrafSync(docsFolder)) throw new Error(`Failed to clean docs folder: "${docsFolder}"`);
-		console.log("Cleaned docs folder".padEnd(21), "=>", path.relative(rootDir, docsFolder));
+	const rootDir = path.join(__dirname, "..", "..");
+
+	// Download documentation to a new folder
+	const downloadFolder = path.join(rootDir, "resources", ".docs-download");
+
+	// Delete the prevoius download folder if it exists
+	if (fs.existsSync(downloadFolder)) {
+		console.log("Cleaning up download folder".padEnd(26), "=>", path.relative(rootDir, downloadFolder));
+		if (!rimrafSync(downloadFolder)) throw new Error(`Failed to clean download folder: "${downloadFolder}"`);
 	}
 
-	const luceeDocsPath = path.join(docsFolder, "lucee-docs.zip");
+	// Download documentation from Lucee
+	const luceeDocsPath = path.join(downloadFolder, "lucee-docs.zip");
+	console.log("Downloading Lucee docs".padEnd(26), "=>", path.relative(rootDir, luceeDocsPath));
 	await downloadFile("https://docs.lucee.org/lucee-docs-json-zipped.zip", luceeDocsPath);
-	console.log("Downloaded Lucee docs".padEnd(21), "=>", path.relative(rootDir, luceeDocsPath));
 
-	const cfDocsPath = path.join(docsFolder, "cfdocs.zip");
+	// Download documentation from CFDocs
+	const cfDocsPath = path.join(downloadFolder, "cfdocs.zip");
+	console.log("Downloading CFDocs".padEnd(26), "=>", path.relative(rootDir, cfDocsPath));
 	await downloadDocsWebsite("https://raw.githubusercontent.com/foundeo/cfdocs/master/data/en/", cfDocsPath);
-	console.log("Downloaded CFDocs".padEnd(21), "=>", path.relative(rootDir, cfDocsPath));
+
+	// Backup the existing docs folder in case we need to restore it
+	const docsFolder = path.join(rootDir, "resources", "docs");
+	if (fs.existsSync(docsFolder)) {
+		// If the documentation is already up to date, skip the rest
+		if (await foldersMatch(downloadFolder, docsFolder)) {
+			console.log("No changes detected in downloaded docs, documentation is up to date.");
+			return;
+		}
+
+		const oldDocsPath = path.join(rootDir, "resources", ".docs-old");
+		fs.mkdirSync(oldDocsPath, { recursive: true });
+		let i = 1;
+		let oldDocsDestination: string;
+		do {
+			oldDocsDestination = path.join(oldDocsPath, `docs-${i.toString().padStart(3, "0")}`); ;
+			i++;
+		} while (fs.existsSync(oldDocsDestination));
+
+		console.log("Backing up docs folder".padEnd(26), "=>", path.relative(rootDir, oldDocsDestination));
+		fs.renameSync(docsFolder, oldDocsDestination);
+	}
+
+	// Make the download folder the new docs folder
+	console.log("Moving download folder".padEnd(26), "=>", path.relative(rootDir, docsFolder));
+	fs.renameSync(downloadFolder, docsFolder);
+
+	console.log("Documentation updated.");
 }
 void main();
 
@@ -92,4 +124,72 @@ async function zipFolder(folderPath: string, zipFilePath: string) {
 	// By using STORE for 0% compression here, we allow the VSIX compression to operate more efficiently for a smaller final size.
 	const zipFile = await zip.generateAsync({ type: "nodebuffer", compression: "STORE" });
 	fs.writeFileSync(zipFilePath, zipFile);
+}
+
+/**
+ * Compares two folders to see if they contain the same files and content.
+ *
+ * - Differences in timestamps are ignored.
+ * - Zip files are compared by their contents, ignoring timestamp and compression differences.
+ * @param folderPathA
+ * @param folderPathB
+ * @returns
+ */
+async function foldersMatch(folderPathA: string, folderPathB: string): Promise<boolean> {
+	assert(fs.statSync(folderPathA).isDirectory(), `Expected folderPathA to be a directory, got "${folderPathA}"`);
+	assert(fs.statSync(folderPathB).isDirectory(), `Expected folderPathB to be a directory, got "${folderPathB}"`);
+	assert(folderPathA !== folderPathB, `Expected folderPathA and folderPathB to be different, got "${folderPathA}" and "${folderPathB}"`);
+
+	const filesA = fs.readdirSync(folderPathA);
+	const filesB = fs.readdirSync(folderPathB);
+	if (filesA.length !== filesB.length) {
+		return false;
+	}
+	for (let i = 0; i < filesA.length; i++) {
+		const fileA = filesA[i];
+		const fileB = filesB[i];
+		if (fileA !== fileB) {
+			return false;
+		}
+		const filePathA = path.join(folderPathA, fileA);
+		const filePathB = path.join(folderPathB, fileB);
+
+		if (!await zipsMatch(filePathA, filePathB)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+/**
+ * Compares two zip files to see if they contain the same files and content.
+ *
+ * - Differences in timestamps are ignored.
+ * @param zipFilePathA
+ * @param zipFilePathB
+ * @returns
+ */
+async function zipsMatch(zipFilePathA: string, zipFilePathB: string): Promise<boolean> {
+	assert(zipFilePathA.endsWith(".zip"), `Expected zipFilePathA to end with ".zip", got "${zipFilePathA}"`);
+	assert(zipFilePathB.endsWith(".zip"), `Expected zipFilePathB to end with ".zip", got "${zipFilePathB}"`);
+
+	const zipA = await JSZip.loadAsync(fs.readFileSync(zipFilePathA));
+	const zipB = await JSZip.loadAsync(fs.readFileSync(zipFilePathB));
+
+	const filesA = Object.keys(zipA.files);
+	const filesB = Object.keys(zipB.files);
+	if (filesA.length !== filesB.length) {
+		return false;
+	}
+	for (const file of filesA) {
+		if (!filesB.includes(file)) {
+			return false;
+		}
+		const fileA = await zipA.files[file].async("string");
+		const fileB = await zipB.files[file].async("string");
+		if (fileA !== fileB) {
+			return false;
+		}
+	}
+	return true;
 }
