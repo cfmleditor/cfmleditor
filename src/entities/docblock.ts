d9250bdc464988e26bc5b43c8454ad9b32b07b47
Gareth Edwards <ghedwards@gmail.com>
2025-07-07 23:08:47 +1000

    Doc block parsing improvements

--- src/entities/docblock.ts
+++ src/entities/docblock.ts
@@ -1,7 +1,11 @@
 import { Range, TextDocument } from "vscode";
 
 // If the key has no value, the last letter is ignored
-const DOC_PATTERN: RegExp = /(\n\s*(?:\*[ \t]*)?(?:@(\w+)(?:\.(\w+))?)?[ \t]*)(\S.*)/gi;
+// const DOC_PATTERN: RegExp = /(\n\s*(?:\*[ \t]*)?(?:@(\w+)(?:[. ](\w+))?)?[ \t]*)(\S.*)/gi;
+
+const DOC_PATTERN: RegExp = /(\n\s*(?:\*)?)(?:(?:[ \t]*(@(\w+))[ \t]+(\w+))|([ \t]*[^@\s].*)?)(.*)/gi;
+const INDENT_PATTERN: RegExp = /^[ \t]{2,}/i;
+const CODE_BLOCK_PATTERN: RegExp = /```/i;
 
 export interface DocBlockKeyValue {
 	key: string; // lowercased
@@ -27,12 +31,16 @@ export function parseDocBlock(document: TextDocument, docRange: Range): DocBlock
 	let activeValueStartOffset = 0;
 	let activeValueEndOffset = 0;
 	let docBlockMatches: RegExpExecArray | null;
+	let intendedCodeBlock: boolean = false;
+	let explicitCodeBlock: boolean = false;
+	let metadataValueIndent: RegExpMatchArray | null;
+	let metadataValueReplace: RegExp | undefined;
 	const docBlockOffset: number = document.offsetAt(docRange.start);
 	while ((docBlockMatches = DOC_PATTERN.exec(docBlockStr))) {
-		const valuePrefix: string = docBlockMatches[1];
-		const metadataKey: string = docBlockMatches[2];
-		const metadataSubkey: string = docBlockMatches[3];
-		const metadataValue: string = docBlockMatches[4];
+		const valuePrefix: string = docBlockMatches[0] || "";
+		const metadataKey: string = docBlockMatches[3] || "";
+		const metadataValue: string = docBlockMatches[6] || docBlockMatches[5] || "";
+		const metadataSubkey: string = docBlockMatches[4] || "";
 		const docValueOffset: number = docBlockOffset + docBlockMatches.index + valuePrefix.length;
 
 		if (metadataKey) {
@@ -44,15 +52,25 @@ export function parseDocBlock(document: TextDocument, docRange: Range): DocBlock
 				activeSubkey = undefined;
 			}
 		}
-		else if (metadataValue === "*") {
+		else if (metadataValue === "") {
 			continue;
 		}
 
 		if ((activeKey !== prevKey || activeSubkey !== prevSubkey) && activeValue) {
+			// Close code blocks if they're open
+			if (explicitCodeBlock) {
+				explicitCodeBlock = false;
+			}
+			else if (intendedCodeBlock) {
+				intendedCodeBlock = false;
+				metadataValueReplace = undefined;
+				activeValue += "\n```";
+			}
+
 			docBlock.push({
 				key: prevKey,
 				subkey: prevSubkey,
-				value: activeValue,
+				value: activeValue.trim(),
 				valueRange: new Range(document.positionAt(activeValueStartOffset), document.positionAt(activeValueEndOffset)),
 			});
 			prevKey = activeKey;
@@ -60,13 +78,34 @@ export function parseDocBlock(document: TextDocument, docRange: Range): DocBlock
 			activeValue = undefined;
 		}
 
-		if (activeValue) {
-			activeValue += " " + metadataValue;
-		}
-		else {
+		if (!activeValue) {
 			activeValueStartOffset = docValueOffset;
-			activeValue = metadataValue;
+			activeValue = "";
 		}
+
+		if (CODE_BLOCK_PATTERN.test(metadataValue)) {
+			explicitCodeBlock = !explicitCodeBlock;
+		}
+
+		if (!explicitCodeBlock) {
+			metadataValueIndent = metadataValue.match(INDENT_PATTERN);
+			if (!intendedCodeBlock && metadataValueIndent && metadataValueIndent.length > 0) {
+				metadataValueReplace = new RegExp("^(" + metadataValueIndent[0] + ")");
+				intendedCodeBlock = true;
+				activeValue += "\n```typescript";
+			}
+			else if (intendedCodeBlock && (!metadataValueIndent || metadataValueIndent.length < 1)) {
+				intendedCodeBlock = false;
+				metadataValueReplace = undefined;
+				activeValue += "\n```";
+			}
+		}
+
+		if (activeValue) {
+			activeValue += "\n";
+		}
+
+		activeValue += metadataValueReplace ? metadataValue.replace(metadataValueReplace, "") : metadataValue;
 		activeValueEndOffset = docValueOffset + metadataValue.length;
 	}
 
@@ -74,7 +113,7 @@ export function parseDocBlock(document: TextDocument, docRange: Range): DocBlock
 		docBlock.push({
 			key: activeKey,
 			subkey: activeSubkey,
-			value: activeValue,
+			value: activeValue.trim(),
 			valueRange: new Range(document.positionAt(activeValueStartOffset), document.positionAt(activeValueEndOffset)),
 		});
 	}
--- src/test/workspace/cfscript/Gizmo.cfc
+++ src/test/workspace/cfscript/Gizmo.cfc
@@ -13,6 +13,23 @@ component extends="cfscript.Base" {
 		return "Hello " & variables.name;
 	}
 
+	/**
+     * Example function that performs a boolean operation.
+     * 
+     * This function is a placeholder for demonstrating how to use the `example` function.
+     * 
+     * List:
+     * - Foo
+     * - Bar
+     * 
+     * Example usage:
+     *     if (example("template.cfm", 42)) {
+     *         // Do something if the condition is true
+     *     }
+     * @param template [required] The template string to be used.
+     * @param line The line number as a numeric value.
+     * @return Returns true.
+     */
 	static function staticGenerateID() {
 		return "Hello World";
 	}
